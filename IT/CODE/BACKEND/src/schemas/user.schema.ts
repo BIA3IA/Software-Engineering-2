cae76b921fdc81b4e4457e6d015954136b483dfd
SimoneErrigo <simone.errigo@mail.polimi.it>
2025-12-19 20:14:22 +0100

    Adding another field to check if the current password is the real one whe updating password

--- IT/CODE/BACKEND/src/schemas/user.schema.ts
+++ IT/CODE/BACKEND/src/schemas/user.schema.ts
@@ -3,6 +3,7 @@ import Joi from 'joi';
 export const updateProfileSchema = Joi.object({
     username: Joi.string().alphanum().min(3).max(20),      
     email: Joi.string().email(),
+    currentPassword: Joi.string().min(8),
     password: Joi.string().min(8),
     systemPreferences: Joi.array().items(Joi.string())
 });
